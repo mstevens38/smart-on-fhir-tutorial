89c9e8f48eac5e48075250c761873b963f995188
mstevens38 <60853463+mstevens38@users.noreply.github.com>
2020-02-09 15:22:07 -0500

    Update example-smart-app.js

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -43,7 +43,7 @@
           var hdl = byCodes('2085-9');
           var ldl = byCodes('2089-1');
 
-          var weight = byCodes('29463-7')
+          var weight = byCodes('29463-7');
           
           var p = defaultPatient();
           p.birthdate = patient.birthDate;
